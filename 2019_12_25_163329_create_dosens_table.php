98df87213a1dffcb39b9e72cd32ee827dfc5cdf3
SumainatulUkhroh <59229364+SumainatulUkhroh@users.noreply.github.com>
2019-12-25 11:10:42 -0800

    Add files via upload

---
+++ 2019_12_25_163329_create_dosens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDosensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+             $table->char('NIM_dosen', 25) ->unique();
+              $table->string('nama', 100);
+              $table->string('alamat',);
+             
+               
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosens');
+    }
+}
---
+++ 2019_12_25_163514_create_matakuliahs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatakuliahsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matakuliahs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+             $table->char('kd_MataKuliah', 25) ->unique();
+               $table->char('jmlh_SKS', 4);
+               
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matakuliahs');
+    }
+}
